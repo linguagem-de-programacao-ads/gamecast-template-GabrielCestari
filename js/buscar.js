7bbf41d8496cb2442c0955e588f4143d85c64bc6
GabrielCestari <gabriel.sanchez@sptech.school>
2024-04-18 21:22:59 -0300

    tela jogos

--- js/buscar.js
+++ js/buscar.js
@@ -1,6 +1,5 @@
 async function buscar(){
-    console.log("passei por aqui")
-
+  
    const resposta = await fetch("https://660f44b1356b87a55c510dcb.mockapi.io/agendas");
    const respostaDadosAgenda = await resposta.json();
 
@@ -9,11 +8,35 @@ async function buscar(){
    const cards = document.getElementById("cards_games")
 
    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-    return "TESTE "
-   });
+    return `
+    <div class="cardItem">
+    <div class="dataGame"> 
+        <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+    </div>
+    <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+    <div class="descricao">
+        <p> <strong>${itemAgenda.nome}</strong> </p>
+        <p> ${itemAgenda.Descricao}</p>
+        <p><strong>Gamers: </strong></p>
+
+        <div class="gamers">
+        ${
+            itemAgenda.gamers.map((gamerItem) => {
+                return` <div class="gamerItem">@${gamerItem}</div>`
+            }).join('')
+        }
+           
+        </div>
+
+      
+    </div>  
+    <div class="assistir">
+            <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+    </div>
+</div>
+    `
+   }).join('');
 }
 
-
-console.log("antes de buscar")
 buscar()
-console.log("depois de buscar")
+
